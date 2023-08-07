3d3178f1a9a1bc79b371d023bd4524f19d5f2105
zhangwj <wenjie.zhang@palmpay-inc.com>
2023-08-07 16:36:47 +0800

    使用ksp

--- app/src/main/java/com/smart/zwj/zroute/MainActivity.kt
+++ app/src/main/java/com/smart/zwj/zroute/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.core.view.WindowCompat
 
 @ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             ZRouteNavigation().NavigationView()
         }
